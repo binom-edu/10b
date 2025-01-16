dc1627efcb6416cf4f18d8a5a990ff07024b998e
klindyuk <klindyuk@gmail.com>
2025-01-16 15:37:35 +0300

    3

---
+++ 3/2.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+ans = a
+if b > ans:
+    ans = b
+if c > ans:
+    ans = c
+print(ans)
---
+++ 3/3.py
@@ -0,0 +1,10 @@
+# Какое больше?
+a = int(input())
+b = int(input())
+
+if a > b:
+    print('Первое больше')
+elif a == b:
+    print('Равны')
+else:
+    print('Второе больше')
